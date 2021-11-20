aabd10de0e11e8cf587af4a701d0647b146e2783
jr101.dallas <jr101.dallas@gmail.com>
2021-11-20 01:37:17 -0600

    offenseModsExtensions tests

---
+++ PhysicsTests/Extensions/OffenseModsExtensionsTests.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Physics;
+using Physics.Components;
+using Physics.Items;
+using Physics.Models;
+
+namespace PhysicsTest
+{
+    [TestClass]
+    public class OffenseModsExtensionsTests
+    {
+        [TestMethod]
+        public void AddOffenseModsSumsValues()
+        {
+            var off1 = new OffenseMods(){totalAim = 18, totalDamage = 11};
+            var off2 = new OffenseMods(){totalAim = 12, totalDamage = 9};
+            off1.Add(off2);
+            Assert.AreEqual(30, off1.totalAim);
+            Assert.AreEqual(20, off1.totalDamage);
+        }
+
+        [TestMethod]
+        public void AddItemSumsValues()
+        {
+            var off1 = new OffenseMods(){totalAim = 18, totalDamage = 11};
+            var item = new Weapon();
+            off1.Add(item);
+            Assert.AreEqual(23, off1.totalAim);
+            Assert.AreEqual(16, off1.totalDamage);
+        }
+
+        [TestMethod]
+        public void AddOffenseSumsValues()
+        {
+            var off1 = new OffenseMods(){totalAim = 18, totalDamage = 11};
+            var offense = new Offense();
+            off1.Add(offense);
+            Assert.AreEqual(21, off1.totalAim);
+            Assert.AreEqual(16, off1.totalDamage);
+        }
+    }
+}
